2b4a8edb08581b362ac7738223e83d1d8fa6e5b3
ElenQa <47454865+ElenQa@users.noreply.github.com>
2019-06-18 16:29:25 +0300

    Add project files.

---
+++ Shape_param/Main_M.cs
@@ -0,0 +1,37 @@
+﻿using Shape_param;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shape_param
+{
+    public class Main_M
+    {
+
+        public static void Main(string[] args)
+        {
+            int tr_side1 = 0;
+            int tr_side2 = 0;
+            int tr_side3 = 0;
+            double tr_angle = 0;
+
+            foreach (string param in args)
+            {
+                //Console.WriteLine(param);
+                tr_side1 = Convert.ToInt32(args[0]);
+                tr_side2 = Convert.ToInt32(args[1]);
+                tr_side3 = Convert.ToInt32(args[2]);
+                tr_angle = Convert.ToInt32(args[3]);
+            }
+
+            Shape tr = new Triangle1(tr_side1, tr_side2, tr_side3, tr_angle, "Triangle");
+
+            Console.WriteLine(tr);
+            Console.ReadLine();
+        }
+    }
+}
+
+
---
+++ Shape_param/Triangle.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Shape_param;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shape_param
+{
+    class Triangle1 : Shape
+    {
+        private int side1;
+        private int side2;
+        private int side3;
+        private double angle;
+
+
+        public Triangle1 (int side1, int side2, int side3, double angle, string shapeId) : base(shapeId)
+        {
+            this.side1 = side1;
+            this.side2 = side2;
+            this.side3 = side3;
+            this.angle = angle;
+        }
+        public override double Area
+        {
+            get
+            {
+                return 0.5 * (side1 * side2) * Math.Sin(angle);
+            }
+        }
+        public override double Perimeter
+        {
+            get
+            {
+                return side1 + side2 + side3;
+            }
+        }
+    }
+}
